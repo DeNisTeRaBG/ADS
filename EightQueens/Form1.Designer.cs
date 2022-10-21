4ceff05324da63d2e853356094fa3474b649c5d2
DeNisTeRaBG <78407839+DeNisTeRaBG@users.noreply.github.com>
2022-10-21 11:46:45 +0300

    Fortnite in chess

---
+++ EightQueens/Form1.Designer.cs
@@ -0,0 +1,149 @@
+﻿
+namespace EightQueens
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "N =";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 72);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(123, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Numbar of Placements =";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 131);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Placements";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(247, 131);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(94, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Current Placement";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(72, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(15, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(230, 277);
+            this.listBox1.TabIndex = 5;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(251, 147);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(236, 277);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(225, 31);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(513, 450);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ EightQueens/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EightQueens
+{
+    public partial class Form1 : Form
+    {
+        private int N;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        { 
+            
+        }
+
+        bool IsStrike(int x1, int y1, int x2, int y2)
+        {
+            if ((x1 == x2) || (y1 == y2)) return true;
+
+            int tx, ty;
+
+            tx = x1 - 1; ty = y1 - 1;
+            while ((tx >= 1) && (ty == y2)) return true;
+            {
+                if ((tx == x2) && (ty == y2)) return true;
+                tx--; ty--;
+            }
+            
+
+            tx = x1 + 1; ty = y1 + 1;
+            while ((tx <= N) && (ty <= N))
+            {
+                if ((tx == x2) && (ty == y2)) return true;
+                tx++; ty++;
+            }
+
+            tx = x1 + 1; ty = y1 - 1;
+            while ((tx <= N) && (ty >= 1))
+            {
+                if ((tx == x2) && (ty == y2)) return true;
+                tx++; ty--;
+            }
+
+            tx = x1 - 1; ty = y1 + 1;
+            while ((tx >= 1) && (ty <= N))
+            {
+                if ((tx == x2) && (ty == y2)) return true;
+                tx--; ty++;
+            }
+            return false;
+        }
+
+        bool Strike(int[] M, int p)
+        {
+            int px, py, x, y;
+            int i;
+
+            px = M[p];
+            py = p;
+
+            for (i = 1; i <= p - 1; i++)
+            {
+                x = M[i];
+                y = i;
+                if (IsStrike(x, y, px, py))
+                    return true;
+            }
+            return false;
+        }
+
+        void InitDataGriView(int N)
+        {
+            int i;
+
+            dataGridView1.Columns.Clear();
+
+            for (i = 1; i <= N; i++)
+            {
+                dataGridView1.Columns.Add("i" + i.ToString(), i.ToString());
+
+                dataGridView1.Columns[i - 1].Width = 30;
+            }
+
+            dataGridView1.Rows.Add(N);
+
+            for (i = 1; i <= N; i++)
+                dataGridView1.Rows[i - 1].HeaderCell.Value = i.ToString();
+
+            dataGridView1.RowsDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+
+        }
+
+        void ShowDataGridView(string s, int N)
+        {
+            int i;
+            int j;
+            string xs, ys;
+            int x, y;
+
+            for (i = 0; i < N; i++)
+                for (j = 0; j < N; j++)
+                    dataGridView1.Rows[i].Cells[j].Value = "";
+
+            j = 0;
+            for (i = 0; i < N; i++)
+            {
+                xs = "";
+                while (s[j] != ',')
+                {
+                    xs = xs + s[j].ToString();
+                    j++;
+                }
+
+                j++;
+
+                ys = "";
+                while (s[j] != '-')
+                {
+                    ys = ys + s[j].ToString();
+                    j++;
+                }
+
+                j++;
+
+                x = Convert.ToInt32(xs);
+                y = Convert.ToInt32(ys);
+
+                dataGridView1.Rows[y - 1].Cells[x - 1].Value = "X";
+            }
+        }
+
+        void AddToListBox(int[] M, int N)
+        {
+            string s = "";
+            for (int i = 1; i <= N; i++)
+                s = s + M[i].ToString() + "," + i.ToString() + "-";
+            listBox1.Items.Add(s);
+        }
+
+        private void listBox1_Click(object sender, EventArgs e)
+        {
+            if (listBox1.Items.Count <= 0) return;
+            int num;
+            string s;
+            num = listBox1.SelectedIndex;
+            s = listBox1.Items[num].ToString();
+            ShowDataGridView(s, N);
+        }
+
+        private void listBox1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (listBox1.Items.Count <= 0) return;
+            int num;
+            string s;
+            num = listBox1.SelectedIndex;
+            s = listBox1.Items[num].ToString();
+            ShowDataGridView(s, N);
+        }
+
+        private void button1_Click(object sender, KeyEventArgs e)
+        {
+            const int MaxN = 20;
+            int[] M = new int[MaxN];
+            int p;
+            int k;
+
+            N = Convert.ToInt32(textBox1.Text);
+
+            InitDataGriView(N);
+
+            listBox1.Items.Clear();
+
+            p = 1;
+            M[p] = 0;
+            M[0] = 0;
+            k = 0;
+
+            while (p > 0)
+            {
+                M[p] = M[p] + 1;
+                if (p == N)
+                {
+                    if (M[p] > N)
+                    {
+                        while (M[p] > N) p--;
+                    }
+                    else
+                    {
+                        if (M[p] > N)
+                        {
+                            while (M[p] > N) p--;
+                        }
+                        else 
+                        {
+                            if (!Strike(M, p))
+                            {
+                                p++;
+                                M[p] = 0;
+                            }
+                        }
+                    }
+
+                }
+                if (k > 0)
+                {
+                    listBox1.SelectedIndex = 0;
+                    listBox1_Click(sender, e);
+                    label2.Text = "Number of placements =" + k.ToString();
+                }
+            }
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            const int MaxN = 20;
+            int[] M = new int[MaxN];
+            int p;
+            int k;
+
+            N = Convert.ToInt32(textBox1.Text);
+
+            InitDataGriView(N);
+
+            listBox1.Items.Clear();
+
+            p = 1;
+            M[p] = 0;
+            M[0] = 0;
+            k = 0;
+
+            while (p > 0)
+            {
+                M[p] = M[p] + 1;
+                if (p == N)
+                {
+                    if (M[p] > N)
+                    {
+                        while (M[p] > N) p--;
+                    }
+                    else
+                    {
+                        if (M[p] > N)
+                        {
+                            while (M[p] > N) p--;
+                        }
+                        else
+                        {
+                            if (!Strike(M, p))
+                            {
+                                p++;
+                                M[p] = 0;
+                            }
+                        }
+                    }
+
+                }
+                if (k > 0)
+                {
+                    listBox1.SelectedIndex = 0;
+                    listBox1_Click(sender, e);
+                    label2.Text = "Number of placements =" + k.ToString();
+                }
+            }
+        }
+    }
+}
